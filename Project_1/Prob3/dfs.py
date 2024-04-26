9ccc8fe7b8648c589fbc94acf5ff34f12e1a44ab
wuwen <1056374449@qq.com>
2024-04-26 10:41:50 +0800

    穿墙

--- Project_1/Prob3/dfs.py
+++ Project_1/Prob3/dfs.py
@@ -6,12 +6,13 @@ import matplotlib.pyplot as plt
 import copy
 from typing import List, Any, Tuple
 from heapq import *
+import time
 
 
-def visualize_maze_with_path(maze, path):
-    plt.figure(figsize=(len(maze[0]), len(maze)))  # 设置图形大小
+def visualize_maze_with_path(maze, path, title: str):
+    # figure=plt.figure(figsize=(len(maze[0]), len(maze)))  # 设置图形大小
     plt.imshow(
-        maze, cmap="Greys", interpolation="nearest", vmin=0, vmax=3
+        maze, cmap="Accent", interpolation=None, vmin=0, vmax=6
     )  # 使用灰度色图，并关闭插值
 
     # 绘制路径
@@ -27,7 +28,10 @@ def visualize_maze_with_path(maze, path):
     plt.grid(which="minor", color="black", linestyle="-", linewidth=2)
 
     plt.axis("on")  # 显示坐标轴
-    plt.show()
+
+    plt.title(title)
+    # input()
+    plt.pause(0.1)
 
 
 class State:
@@ -55,6 +59,7 @@ class Maze:
             self.maze_original.append(a)
         self.maze = copy.deepcopy(self.maze_original)
         self.path = []
+        figure = plt.figure(figsize=(len(self.maze[0]), len(self.maze)))  # 设置图形大小
 
     def __str__(self) -> str:
         res = ""
@@ -65,6 +70,7 @@ class Maze:
         return res
 
     def run(self, type: str):
+        plt.close()
         self.path = []
         self.maze = copy.deepcopy(self.maze_original)
         if type == "dfs":
@@ -75,12 +81,14 @@ class Maze:
             self.dijikstra()
         elif type == "A_star":
             self.A_star()
+        elif type == "dijikstra_wall":
+            self.dijikstra_wall()
 
-    def visualize(self):
+    def visualize(self, title: str):
         print(self.path)
         for a in self.maze:
             print(a)
-        visualize_maze_with_path(self.maze, self.path)
+        visualize_maze_with_path(self.maze, self.path, title)
 
     def next(self, pos: tuple) -> list[tuple]:
         dx = [1, 0, -1, 0]
@@ -94,23 +102,34 @@ class Maze:
                 and nx <= self.n - 1
                 and 0 <= ny
                 and ny <= self.m - 1
-                and self.maze_original[nx][ny] != 3
+                # and self.maze_original[nx][ny] != 3
             ):
                 res.append((nx, ny))
         return res
 
     def dfs(self, now: tuple, post: tuple, path: List = []):
-        self.maze[now[0]][now[1]] = 2
+        self.maze[now[0]][now[1]] = (
+            self.maze[now[0]][now[1]] + 1 if self.maze[now[0]][now[1]] != 0 else 4
+        )
+        self.visualize(title="DFS")
         # print(path+[now])
+        if len(self.path)!=0 and len(path)>len(self.path):
+            self.maze[now[0]][now[1]] = (
+                self.maze[now[0]][now[1]] - 1 if self.maze[now[0]][now[1]] != 4 else 0
+            )
+            return
         if now == (self.n - 1, self.m - 1):
             if len(path + [now]) < len(self.path) or len(self.path) == 0:
                 self.path = path + [now]
             return
         next_list = self.next(now)
         for next in next_list:
-            if next in path:
+            if next in path or self.maze[next[0]][next[1]]!=0:
                 continue
             self.dfs(next, now, path + [now])
+        self.maze[now[0]][now[1]] = (
+            self.maze[now[0]][now[1]] - 1 if self.maze[now[0]][now[1]] != 4 else 0
+        )
         return
 
     def bfs(self):
@@ -118,6 +137,7 @@ class Maze:
         heappush(pq, State(self.Manhattan((0, 0)), 0, [(0, 0)]))
         self.maze[0][0] = 2
         while len(pq) != 0:
+            self.visualize(title="BFS")
             state: State = heappop(pq)
             nowx, nowy = state.path[-1]
             # print(nowx, nowy)
@@ -145,11 +165,16 @@ class Maze:
         x, y = pos
         return abs(self.n - 1 - x) + abs(self.m - 1 - y)
 
+    def L2(self,pos:tuple):
+        x,y=pos
+        return (self.n-1-x)**2 + (self.n-1-y)**2
+
     def dijikstra(self):
         pq = []
         heappush(pq, State(0, 0, [(0, 0)]))
         self.maze[0][0] = 2
         while len(pq) != 0:
+            self.visualize(title="DIJIKSTRA")
             state: State = heappop(pq)
             nowx, nowy = state.path[-1]
             print(nowx, nowy)
@@ -176,12 +201,62 @@ class Maze:
                         state.path + [(nx, ny)],
                     ),
                 )
+    
+    def dijikstra_wall(self):
+        pq = []
+        heappush(pq, State(0, 0, [(0, 0)]))
+        self.maze[0][0] = 2
+        while len(pq) != 0:
+            self.visualize(title="DIJIKSTRA_WALL")
+            state: State = heappop(pq)
+            nowx, nowy = state.path[-1]
+            # print(nowx, nowy)
+            self.maze[nowx][nowy] = 2
+            if (
+                nowx == self.n - 1
+                and nowy == self.m - 1
+                # and (len(state.path) < len(self.path) or len(self.path) == 0)
+            ):
+                self.path = state.path
+                return
+            next_list = self.next((nowx, nowy))
+            for nx, ny in next_list:
+                # if self.maze[nx][ny] != 0:
+                #     continue
+                # if (nx,ny) in state.path:
+                #     continue
+                if self.maze[nx][ny]==3:
+                    tx=2*nx-nowx
+                    ty=2*ny-nowy
+                    if (tx<0 or tx>=self.n or ty<0 or ty>=self.m) or self.maze[tx][ty]!=0:
+                        continue
+                    self.maze[tx][ty]=1
+                    heappush(
+                        pq,
+                        State(
+                            state.step+7,
+                            state.step+1,
+                            state.path+[(tx,ty)]
+                        )
+                    )
+                if self.maze[nx][ny] != 0:
+                    continue
+                self.maze[nx][ny] = 1
+                heappush(
+                    pq,
+                    State(
+                        state.step + 1,
+                        state.step + 1,
+                        state.path + [(nx, ny)],
+                    ),
+                )
 
     def A_star(self):
         pq = []
         heappush(pq, State(self.Manhattan((0, 0)), 0, [(0, 0)]))
         self.maze[0][0] = 2
         while len(pq) != 0:
+            self.visualize(title="A*")
             state: State = heappop(pq)
             nowx, nowy = state.path[-1]
             print(nowx, nowy)
@@ -200,6 +275,7 @@ class Maze:
                     pq,
                     State(
                         self.Manhattan((nx, ny)) + state.step + 1,
+                        # self.L2((nx, ny)) + state.step + 1,
                         state.step + 1,
                         state.path + [(nx, ny)],
                     ),
@@ -207,20 +283,27 @@ class Maze:
 
 
 if __name__ == "__main__":
+    plt.ion()
+
     maze = Maze()
     print(maze)
 
     # maze.run("dfs")
-    # maze.visualize()
+    # maze.visualize(title="DFS")
 
-    maze.run("bfs")
-    maze.visualize()
+    # maze.run("dijikstra_wall")
+    # maze.visualize(title="dijikstra_wall")
+    # input()
 
-    maze.run("dijikstra")
-    maze.visualize()
-
+    # maze.run("bfs")
+    # maze.visualize(title="BFS")
+    # input()
+    # maze.run("dijikstra")
+    # maze.visualize(title="DIJIKSTRA")
+    # input()
     maze.run("A_star")
-    maze.visualize()
+    maze.visualize(title="A*")
+    input()
 """
 5 5
 0 1 0 0 0
@@ -250,11 +333,11 @@ if __name__ == "__main__":
 9 9
 0 0 0 0 0 0 0 0 0 
 0 0 1 0 0 0 1 0 0
-0 0 1 0 0 0 1 0 0
-0 0 1 0 0 0 1 0 0
-0 0 1 0 0 0 1 0 0
-0 0 1 0 0 0 1 0 0
-0 0 1 1 1 1 1 0 0
-0 0 1 0 0 0 0 0 0
-0 0 1 0 0 0 0 0 0
+0 0 1 0 0 0 1 1 0
+0 1 1 0 1 0 0 0 0
+0 0 1 0 0 0 1 1 0
+0 0 0 0 0 0 1 0 0
+1 0 1 1 1 0 1 0 1
+0 0 0 0 1 0 0 1 0
+0 0 1 0 0 1 0 0 0
 """
